05b29a92954f70a5c33d8d7868d2424c6524dd3b
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-23 13:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function BoardMember(name, homeState, training){
   this.approve   = function(){return "You can do that!"};
   this.doCharity = function(){return "I like to help people."};
   
-  this.relesePressStatement = function(){
+  this.releasePressStatement = function(){
     return "You will see great things from Scuber.";
   };
   
